dbfb5fd9baff245be7ea4e5482cddbbc4fff4979
theandrew168 <andrew@shallowbrooksoftware.com>
2021-10-05 21:03:30 -0500

    Initial work on basic control loop

--- main.go
+++ main.go
@@ -1,85 +1,182 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/MichaelS11/go-ads"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
-var (
-	pump1 = rpio.Pin(9)
-	pump2 = rpio.Pin(25)
-	pump3 = rpio.Pin(11)
-	pump4 = rpio.Pin(8)
+// calibrated to my Gikfun soil moisture sensor
+// https://www.amazon.com/Gikfun-Capacitive-Corrosion-Resistant-Detection/dp/B07H3P1NRM
+const (
+	SoilWet = 8500
+	SoilDry = 19000
 )
 
-func main() {
-	err := ads.HostInit()
+type Seahorse struct {
+	sync.Mutex
+
+	sensor *ads.ADS
+	pump   rpio.Pin
+
+	soilMoisture prometheus.Gauge
+	pumpUptime   prometheus.Counter
+}
+
+func NewSeahorse(sensor *ads.ADS, pump rpio.Pin) (*Seahorse, error) {
+	// init and register prometheus metrics
+	soilMoisture := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "seahorse_soil_moisture",
+		Help: "Current soil moisture level",
+	})
+	prometheus.MustRegister(soilMoisture)
+
+	pumpUptime := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "seahorse_pump_uptime",
+		Help: "Total pump uptime in milliseconds",
+	})
+	prometheus.MustRegister(pumpUptime)
+
+	seahorse := Seahorse{
+		sensor: sensor,
+		pump:   pump,
+
+		soilMoisture:   soilMoisture,
+		pumpUptime: pumpUptime,
+	}
+	return &seahorse, nil
+}
+
+func (s *Seahorse) ReadMoisture() (float64, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	// check the current moisture level
+	result, err := s.sensor.ReadRetry(5)
 	if err != nil {
-		log.Fatalln(err)
+		return 0, err
 	}
 
-	// create new ads with wanted busName and address. 
-	ads1, err := ads.NewADS("I2C1", 0x48, "")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer ads1.Close()
+	// clamp between 0 and 1
+	moisture := float64(result-SoilWet) / float64(SoilDry-SoilWet)
+	moisture = 1.0 - clamp(moisture, 0.0, 1.0)
 
-	ads1.SetConfigGain(ads.ConfigGain1)
+	return moisture, nil
+}
 
+func (s *Seahorse) RunPump(duration time.Duration) {
+	// run the pump for requested duration
+	s.pump.Low()
+	time.Sleep(duration)
+	s.pump.High()
+}
+
+func (s *Seahorse) TrackMoisture(interval time.Duration) {
+	c := time.Tick(interval)
 	for {
-		result, err := ads1.ReadRetry(5)
+		// check the current moisture level
+		moisture, err := s.ReadMoisture()
 		if err != nil {
-			log.Fatalln(err)
+			// if we see an error here, wait a bit and try again
+			log.Println(err)
+			time.Sleep(5 * time.Second)
+			continue
 		}
-		fmt.Println(result)
-		time.Sleep(time.Second)
-	}
 
+		// update metric
+		s.soilMoisture.Set(moisture)
+
+		// sleep til next loop
+		<-c
+	}
+}
+
+func (s *Seahorse) ControlLoop(interval time.Duration) {
+	c := time.Tick(interval)
+	for {
+		// check the current moisture level
+		moisture, err := s.ReadMoisture()
+		if err != nil {
+			// if we see an error here, wait a bit and try again
+			log.Println(err)
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		// if soil is dry, turn on the pump for a few seconds and update metric
+		if moisture < 0.25 {
+			s.RunPump(5 * time.Second)
+			s.pumpUptime.Add(5000)
+		}
+
+		// sleep til next loop
+		<-c
+	}
+}
+
+func main() {
 	// init gpio
 	if err := rpio.Open(); err != nil {
 		log.Fatalln(err)
 	}
 	defer rpio.Close()
 
-	// init pins
-	pump1.Output()
-	pump1.High()
-	pump2.Output()
-	pump2.High()
-	pump3.Output()
-	pump3.High()
-	pump4.Output()
-	pump4.High()
+	// init gpio pin(s)
+	//	pump0 = 9
+	//	pump1 = 25
+	//	pump2 = 11
+	//	pump3 = 8
+	pump := rpio.Pin(9)
+	pump.Output()
+	pump.High()
 
-	// test pump 1
-	pump1.Low()
-	time.Sleep(time.Second)
-	pump1.High()
+	// setup Adafruit ADS1115 connection
+	err := ads.HostInit()
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// test pump 2
-	pump2.Low()
-	time.Sleep(time.Second)
-	pump2.High()
+	// connect to addr 0x48 on bus 1
+	sensor, err := ads.NewADS("I2C1", 0x48, "")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer sensor.Close()
 
-	// test pump 3
-	pump3.Low()
-	time.Sleep(time.Second)
-	pump3.High()
+	// set gain and mode
+	sensor.SetConfigGain(ads.ConfigGain1)
+	sensor.SetConfigInputMultiplexer(ads.ConfigInputMultiplexerSingle0)
 
-	// test pump 4
-	pump4.Low()
-	time.Sleep(time.Second)
-	pump4.High()
+	// ready the seahorse!
+	seahorse, err := NewSeahorse(sensor, pump)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// serve metrics til the end of days
+	// kick off moisture tracking
+	go seahorse.TrackMoisture(15 * time.Second)
+
+	// kick off control loop
+	go seahorse.ControlLoop(15 * time.Minute)
+
+	// serve metrics forever on the main goroutine
 	log.Println("listening on 0.0.0.0:5000")
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe("0.0.0.0:5000", nil)
+}
+
+func clamp(v, min, max float64) float64 {
+	if v < min {
+		return min
+	}
+	if v > max {
+		return max
+	}
+	return v
 }
